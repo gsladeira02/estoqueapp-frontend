50e004d90627016f6adc5795b7363faec1408223
gsladeira02 <gsladeira02@gmail.com>
2026-04-29 10:52:57 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'EstoqueApp',
+  description: 'Sistema de gestao de estoque',
+  manifest: '/manifest.json',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600;700&family=DM+Mono:wght@400;500&display=swap" rel="stylesheet" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
